f14032af7ddccaaf114c8df42eee02844580a17c
Noe1017 <jixiangli1017@gmail.com>
2024-02-21 17:59:11 +0800

    Fisrt commit

---
+++ kafka/src/main/java/com/atguigu/kafka/producer/CustomProducerAcks.java
@@ -0,0 +1,40 @@
+package com.atguigu.kafka.producer;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringSerializer;
+
+import java.util.Properties;
+
+public class CustomProducerAcks {
+
+    public static void main(String[] args) {
+
+        // 1. 创建kafka生产者的配置对象
+        Properties properties = new Properties();
+
+        // 2. 给kafka配置对象添加配置信息：bootstrap.servers 47.122.43.5
+        properties.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "47.122.43.5:9092");
+
+        // key,value序列化（必须）：key.serializer，value.serializer
+        properties.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        properties.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+
+        // acks
+        properties.put(ProducerConfig.ACKS_CONFIG,"1");
+
+        // 重试次数
+        properties.put(ProducerConfig.RETRIES_CONFIG,3);
+
+        // 3. 创建kafka生产者对象
+        KafkaProducer<String, String> kafkaProducer = new KafkaProducer<>(properties);
+        // 4. 调用send方法,发送消息
+        for (int i = 0; i < 5; i++) {
+            kafkaProducer.send(new ProducerRecord<>("first","atguigu " + i));
+        }
+
+        // 5. 关闭资源
+        kafkaProducer.close();
+    }
+}
---
+++ kafka/src/main/java/com/atguigu/kafka/producer/CustomProducerCallback.java
@@ -0,0 +1,40 @@
+package com.atguigu.kafka.producer;
+
+import org.apache.kafka.clients.producer.*;
+import org.apache.kafka.common.serialization.StringSerializer;
+
+import java.util.Properties;
+
+public class CustomProducerCallback {
+
+    public static void main(String[] args) {
+
+        // 1. 创建kafka生产者的配置对象
+        Properties properties = new Properties();
+
+        // 2. 给kafka配置对象添加配置信息：bootstrap.servers 47.122.43.5
+        properties.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "47.122.43.5:9092");
+
+        // key,value序列化（必须）：key.serializer，value.serializer
+        properties.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        properties.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+
+        // 3. 创建kafka生产者对象
+        KafkaProducer<String, String> kafkaProducer = new KafkaProducer<>(properties);
+        // 4. 调用send方法,发送消息
+        for (int i = 0; i < 5; i++) {
+            kafkaProducer.send(new ProducerRecord<>("first","atguigu " + i), new Callback(){
+                @Override
+                public void onCompletion(RecordMetadata metadata, Exception exception){
+
+                    if( exception == null){
+                        System.out.println("主题: " + metadata.topic() + "分区 :" + metadata.partition());
+                    }
+                }
+            });
+        }
+
+        // 5. 关闭资源
+        kafkaProducer.close();
+    }
+}
